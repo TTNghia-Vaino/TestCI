49fd405b30390c60cdccf24d3cbc91b4307ad133
TTNghia-Vaino <ttnghia004@gmail.com>
2025-10-28 11:12:23 +0700

    first commit

---
+++ TestCI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestCI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
